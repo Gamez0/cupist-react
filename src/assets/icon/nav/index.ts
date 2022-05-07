3e84fd460432e40c54529efb421c50123d197ac9
dobin <dobinshin@gmail.com>
2022-05-07 18:28:00 +0900

    [chore] assets 추가

---
+++ src/assets/icon/nav/index.ts
@@ -0,0 +1,5 @@
+export { default as ConnectionLogo } from './connection.png';
+export { default as HomeLogo } from './home.png';
+export { default as LiveLogo } from './live.png';
+export { default as MoreLogo } from './more.png';
+export { default as StationLogo } from './station.png';
